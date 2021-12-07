19c2110a8899c0cb8ab5519d67ad4384f5f63c2e
imransuet <imransuet@gmail.com>
2021-12-07 10:25:28 +0100

    Parse template

--- main.go
+++ main.go
@@ -1,32 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"log"
 	"os"
-	"strings"
+	"text/template"
 )
 
-// To pipeliene the output into a html file, type this command "go run main.go > index.html", here the ouput will in a file namedindex.html
-
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	text, _ := reader.ReadString('\n')
-	// convert CRLF to LF
-	text = strings.Replace(text, "\n", "", -1)
-
-	tpl := `
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-	<meta charset="UTF-8">
-	<title>Hello World!</title>
-	</head>
-	<body>
-	<h1>` + text + `</h1>
-	</body>
-	</html>
-	`
-	fmt.Println(tpl)
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
