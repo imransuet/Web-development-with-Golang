4241e2a658003697ec508fbbb454d3083a138156
imransuet <imransuet@gmail.com>
2021-12-07 10:32:04 +0100

    Parse template and create a new html file for the ouput

--- main.go
+++ main.go
@@ -7,12 +7,17 @@ import (
 )
 
 func main() {
+	// tpl is scontainer which contain files
 	tpl, err := template.ParseFiles("tpl.gohtml")
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	err = tpl.Execute(os.Stdout, nil)
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Println("error creating the desired file", err)
+	}
+	defer nf.Close()
+	err = tpl.Execute(nf, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
